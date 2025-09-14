c81118e59805336214b57fda68d79ce19667fc0a
IvanEstradaa <ivanestrada@live.com>
2025-09-14 23:27:28 +1000

    Fixed Volumes fetch and improved script to not use diskutil rename

--- bypass-mdm.sh
+++ bypass-mdm.sh
@@ -11,31 +11,31 @@ NC='\033[0m'
 
 echo -e "${YEL}Bypass MDM from Recovery${NC}"
 
-echo -e "${BLU}Fetching System Volume name${NC}"
-system_volume=$(diskutil info / | grep  "Volume Name" | awk -F': ' '{print $2}' | xargs) # Returns something like: "Macintosh HD"
-echo -e "${GRN}$system_volume${NC}"   
-
-if [ -d "/Volumes/$system_volume - Data" ]; then
-    echo -e "${BLU}Renaming '$system_volume - Data' to 'Data'${NC}"
-    diskutil rename "$system_volume - Data" "Data"
-fi
+echo -e "${BLU}Fetching Data and Root Volume names${NC}"
+data_volume=$(ls -1 /Volumes | grep ' - Data$') # Gets something like 'Macintosh HD - Data'
+echo -e "${GRN}Data Volume: $data_volume${NC}"   
+root_volume=${data_volume% - Data} # keeps only 'Macintosh HD'
+echo -e "${GRN}Root Volume: $root_volume${NC}"   
 
 echo -e "${BLU}Creating Temporary User${NC}"
-real_name="apple"
-username="apple"
-password="apple"
+read -p "Enter Temporary Fullname (Default is 'Apple'): " realName
+real_name="${real_name:=Apple}"
+read -p "Enter Temporary Username (Default is 'Apple'): " username
+username="${username:=Apple}"
+read -p "Enter Temporary Password (Default is '1234'): " passw
+password="${password:=1234}"
+# echo -e "${GRN}Password: $password${NC}" # if automated script
 
-dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
+dscl_path="/Volumes/$data_volume/private/var/db/dslocal/nodes/Default"
 users_path='/Local/Default/Users'
 groups_path='/Local/Default/Groups'
 
-echo -e "${GRN}Password: $password${NC}"
 dscl -f "$dscl_path" localhost -create "$users_path/$username"
 dscl -f "$dscl_path" localhost -create "$users_path/$username" UserShell "/bin/zsh"
 dscl -f "$dscl_path" localhost -create "$users_path/$username" RealName "$real_name"
 dscl -f "$dscl_path" localhost -create "$users_path/$username" UniqueID "501"
 dscl -f "$dscl_path" localhost -create "$users_path/$username" PrimaryGroupID "20"
-mkdir -p "/Volumes/Data/Users/$username"
+mkdir -p "/Volumes/$data_volume/Users/$username"
 dscl -f "$dscl_path" localhost -create "$users_path/$username" NFSHomeDirectory "/Users/$username"
 dscl -f "$dscl_path" localhost -passwd "$users_path/$username" "$password"
 dscl -f "$dscl_path" localhost -append "$groups_path/admin" GroupMembership "$username"
@@ -47,17 +47,18 @@ mdm_domains=(
   "mdm.amazon.com"  "deviceenrollment.amazon.com"  "dexcom.okta.com"  "dexcom.jamfcloud.com"
 )
 for domain in "${mdm_domains[@]}"; do
-  echo "0.0.0.0 $domain" >> /Volumes/"$system_volume"/etc/hosts
+  echo "0.0.0.0 $domain" >> /Volumes/"$root_volume"/etc/hosts
 done
 echo -e "${GRN}Successfully blocked MDM & Profile Domains${NC}"
 
 echo -e "${BLU}Removing configuration profiles${NC}"
-touch /Volumes/Data/private/var/db/.AppleSetupDone
-confprofile_path="/Volumes/$system_volume/var/db/ConfigurationProfiles/Settings"
-rm -rf $confprofile_path/.cloudConfigHasActivationRecord
-rm -rf $confprofile_path/.cloudConfigRecordFound
-touch $confprofile_path/.cloudConfigProfileInstalled
-touch $confprofile_path/.cloudConfigRecordNotFound
+profile_path="/Volumes/$root_volume/var/db/ConfigurationProfiles/Settings"
+
+touch /Volumes/"$data_volume"/private/var/db/.AppleSetupDone
+rm -rf $profile_path/.cloudConfigHasActivationRecord
+rm -rf $profile_path/.cloudConfigRecordFound
+touch $profile_path/.cloudConfigProfileInstalled
+touch $profile_path/.cloudConfigRecordNotFound
 
 echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
 echo -e "${NC}Rebooting your Mac...${NC}"
